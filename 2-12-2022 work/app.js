0365571e86870add6116fcdb1c54c39acd91fd3a
PathwayXR <98002678+PathwayXR@users.noreply.github.com>
2022-02-19 23:16:59 -0500

    2-19-2022 Updates

--- 2-12-2022 work/app.js
+++ 2-12-2022 work/app.js
@@ -37,6 +37,7 @@ class App{
         this.controls.update();
         
         this.stats = new Stats();
+        document.body.appendChild( this.stats.dom )  ;
         
         this.raycaster = new THREE.Raycaster();
         this.workingMatrix = new THREE.Matrix4();
@@ -75,93 +76,51 @@ class App{
             object.position.z = this.random( -2, 2 );
 
             this.room.add( object );
+
         }
-        
-        this.highlight = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.BackSide } ) );
-        this.highlight.scale.set(1.2, 1.2, 1.2);
-        this.scene.add(this.highlight);
     }
     
     setupXR(){
         this.renderer.xr.enabled = true;
         
         const button = new VRButton( this.renderer );
-        
-        const self = this;
-        
+
         this.controllers = this.buildControllers();
-        
-        function onSelectStart() {
-            
-            this.children[0].scale.z = 10;
-            this.userData.selectPressed = true;
-        }
-
-        function onSelectEnd() {
-
-            this.children[0].scale.z = 0;
-            self.highlight.visible = false;
-            this.userData.selectPressed = false;
-            
-        }
-        
-        this.controllers.forEach( (controller) => {
-            controller.addEventListener( 'selectstart', onSelectStart );
-            controller.addEventListener( 'selectend', onSelectEnd );
-        });
-
-
-
     }
     
     buildControllers(){
         const controllerModelFactory = new XRControllerModelFactory();
-        
+
         const geometry = new THREE.BufferGeometry().setFromPoints( [
-            new THREE.Vector3(0,0,0),
-            new THREE.Vector3(0,0,-1)
+                new THREE.Vector3(0,0,0),
+                new THREE.Vector3(0,0,-1)
         ]);
         const line = new THREE.Line( geometry );
-        line.name = 'line';
+        line.name = 'line'
         line.scale.z = 0;
-        
+
         const controllers = [];
-        
+
         for(let i=0; i<=1; i++){
             const controller = this.renderer.xr.getController( i );
-            controller.add( line.clone() );
+            controller.add ( line.clone() );
             controller.userData.selectPressed = false;
-            this.scene.add(controller);
-            
+            this.scene.add(controller)
+
             controllers.push( controller );
-            
+
             const grip = this.renderer.xr.getControllerGrip( i );
             grip.add( controllerModelFactory.createControllerModel( grip ));
             this.scene.add( grip );
-        }
         
+        }
+
         return controllers;
+        
     }
     
     handleController( controller ){
-        if (controller.userData.selectPressed ){
-            controller.children[0].scale.z = 10;
-
-            this.workingMatrix.identity().extractRotation( controller.matrixWorld );
-
-            this.raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld );
-            this.raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( this.workingMatrix );
-
-            const intersects = this.raycaster.intersectObjects( this.room.children );
-
-            if (intersects.length>0){
-                intersects[0].object.add(this.highlight);
-                this.highlight.visible = true;
-                controller.children[0].scale.z = intersects[0].distance;
-            }else{
-                this.highlight.visible = false;
-            }
-        }
+        
     }
     
     resize(){
