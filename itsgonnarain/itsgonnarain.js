decbc0b8a9f85c940231ac555dcc8f301e1b297b
PathwayXR <98002678+PathwayXR@users.noreply.github.com>
2022-08-04 16:48:27 -0400

    Audio Tests 8-4-2022

--- itsgonnarain/itsgonnarain.js
+++ itsgonnarain/itsgonnarain.js
@@ -11,28 +11,28 @@ function go() {
     function startLoop(audioBuffer, pan = 0, rate = 1) {
       let sourceNode = audioContext.createBufferSource();
       let pannerNode = audioContext.createStereoPanner();
-
+    
       sourceNode.buffer = audioBuffer;
       sourceNode.loop = true;
       sourceNode.loopStart = 2.98;
       sourceNode.loopEnd = 3.80;
       sourceNode.playbackRate.value = rate;
       pannerNode.pan.value = pan;
-      
+    
       sourceNode.connect(pannerNode);
       pannerNode.connect(audioContext.destination);
     
       sourceNode.start(0, 2.98);
     }
-
+    
     fetch('audio-file-tf-intro-01.mp3')
-    .then(response => response.arrayBuffer())
-    .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
-    .then(audioBuffer => {
-      startLoop(audioBuffer, -1);
-      startLoop(audioBuffer, 1, 1.002);
-    })
-    .catch(e => console.error(e));
+      .then(response => response.arrayBuffer())
+      .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+      .then(audioBuffer => {
+        startLoop(audioBuffer, -1);
+        startLoop(audioBuffer, 1, 1.002);
+      })
+      .catch(e => console.error(e));
 
     
     
